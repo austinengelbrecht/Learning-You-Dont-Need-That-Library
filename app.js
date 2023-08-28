b84d8823d98c7f04a4bb6d6d2f146bb3abe9a063
Austin <aengelbrecht>
2023-08-28 16:08:14 -0500

    start web component

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import Store from "./services/Store.js";
 import { loadData } from "./services/Order.js";
 import Router from "./services/Router.js";
 
+// Link Web Components
+import { MenuPage } from "./components/MenuPage.js";
+
 window.app = {};
 app.store = Store;
 app.router = Router;
