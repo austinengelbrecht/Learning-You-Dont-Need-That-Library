56370dbef01069e6385afcafbdf26cdf6cbfeee1
Austin <aengelbrecht>
2023-08-28 10:21:52 -0500

    Create router

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import Store from "./services/Store.js";
 import { loadData } from "./services/Order.js";
+import Router from "./services/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Doc Ready!");
