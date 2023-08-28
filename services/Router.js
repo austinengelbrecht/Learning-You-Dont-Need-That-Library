c2f828a972c6593ad32f37ad1874c10d6519b18f
Austin <aengelbrecht>
2023-08-28 15:32:24 -0500

    Finish router history handling

--- services/Router.js
+++ services/Router.js
@@ -8,7 +8,12 @@ const Router = {
         Router.go(url);
       });
     });
-    // Event
+
+    // Event Handling for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+
     // Check the initial URL
     Router.go(location.pathname);
   },
