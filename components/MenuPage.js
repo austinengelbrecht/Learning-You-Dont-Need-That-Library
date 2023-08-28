fc979843a34381d8108ff8981d7fcc0baf547199
Austin <aengelbrecht>
2023-08-28 16:30:38 -0500

    Add styling to components

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,26 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    // Open shadow DOM
+    this.root = this.attachShadow({ mode: "open" });
+
+    //Create <style> tag in dom
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    // Load CSS
+    async function loadCSS() {
+      // Call stylesheet
+      const request = await fetch("/components/MenuPage.css");
+
+      // Get text from stylesheet
+      const css = await request.text();
+
+      //Add styles to <style> element in shadow dom
+      styles.textContent = css;
+    }
+    loadCSS();
   }
 
   // When the component is attached to the DOM
