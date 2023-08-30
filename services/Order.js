e68d610e1c4c8ba960cb65ae253fe4e04820f704
Austin <aengelbrecht>
2023-08-30 14:48:58 -0500

    Fix errors

--- services/Order.js
+++ services/Order.js
@@ -1,17 +1,13 @@
 import { getProductById } from "./Menu.js";
 
 export async function addToCart(id) {
-  const product = getProductById(id);
-
+  const product = await getProductById(id);
   const results = app.store.cart.filter(
-    (productInCart) => productInCart.product.id == id
+    (prodInCart) => prodInCart.product.id == id
   );
-
   if (results.length == 1) {
-    // Product is already in cart
-
-    app.store.cart = app.store.cart.map(
-      (p) => (p.product.id = id ? { ...p, quantity: p.quantity + 1 } : p)
+    app.store.cart = app.store.cart.map((p) =>
+      p.product.id == id ? { ...p, quantity: p.quantity + 1 } : p
     );
   } else {
     app.store.cart = [...app.store.cart, { product, quantity: 1 }];
@@ -19,5 +15,7 @@ export async function addToCart(id) {
 }
 
 export function removeFromCart(id) {
-  app.store.cart = app.store.cart.filter((p) => p.product.id != id);
+  app.store.cart = app.store.cart.filter(
+    (prodInCart) => prodInCart.product.id != id
+  );
 }
